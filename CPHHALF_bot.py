7916791c10eecb018fd71627c3edccf8cc468df6
Madsem2 <161312532+Madsem2@users.noreply.github.com>
2024-06-03 20:47:55 +0200

    Update CPHHALF_bot.py

--- CPHHALF_bot.py
+++ CPHHALF_bot.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python
+import os
+import logging
 from bs4 import BeautifulSoup
 import requests
-import os
+
+logging.basicConfig(level=logging.INFO)  # Set logging level to INFO
 
 def send_slack_notification(webhook_url, message):
-    payload = {
-        "text": message
-    }
+    payload = {"text": message}
     try:
         response = requests.post(webhook_url, json=payload)
         if response.status_code == 200:
-            print("Slack notification sent successfully")
+            logging.info("Slack notification sent successfully")
         else:
-            print(f"Failed to send Slack notification: {response.text}")
+            logging.error(f"Failed to send Slack notification: {response.text}")
     except Exception as e:
-        print(f"Failed to send Slack notification: {e}")
+        logging.error(f"Failed to send Slack notification: {e}")
 
 def check_availability(webhook_url):
     url = "https://secure.onreg.com/onreg2/bibexchange/?eventid=6277"
@@ -26,12 +27,12 @@ def check_availability(webhook_url):
             if unavailable_text not in soup.text:
                 send_slack_notification(webhook_url, "Ticket Available! Check the website: " + url)
             else:
-                print("Tickets are not available at the moment.")
+                logging.info("Tickets are not available at the moment.")
     except Exception as e:
-        print(f"Code encountered an error: {e}")
+        logging.error(f"Code encountered an error: {e}")
 
 if __name__ == "__main__":
     webhook_url = os.environ.get('SLACK_HOOK')  # Retrieve the Slack webhook URL from environment variables
 
-    print("Running script")
+    logging.info("Running script")
     check_availability(webhook_url)
